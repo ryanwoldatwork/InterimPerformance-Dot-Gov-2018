60b1efe76fe00cdfff15f7a6e0bf7f7c55858845
Megan Looff <34106471+mlooff@users.noreply.github.com>
2019-11-22 10:01:58 -0500

    update functionality

---
+++ js/kpi.js
@@ -0,0 +1,72 @@
+if(performance.navigation.type == 2){
+  console.log( 'Page load due to a back button click. Reloading to refresh the Performance.gov report. ' );
+  location.reload(true);
+}
+
+function myFunction() {
+  document.getElementById("wrapper").innerHTML = codeBlock
+}
+
+var codeBlock =   '<h3>Federal Employee Veiwpoint Survey - Improve Performance</h3>' + '<p>The Workforce for the 21st Century CAP Goal works towards effective and efficient mission achievement and improved service to America through enhanced alignment and strategic management of the Federal workforce.</p>' + '<iframe src="https://staging.d2d.gsa.gov/tableau-report-for-embed/11402" id="no-frame-border-1" frameborder="0" style="height:1130px;width:1050px;"></iframe>';
+
+function myFunction1() {
+  document.getElementById("wrapper").innerHTML = codeBlock1
+}
+
+var codeBlock1 =   '<h3>Federal Employee Veiwpoint Survey - Strategic Hiring</h3>' + '<p>The Workforce for the 21st Century CAP Goal works towards effective and efficient mission achievement and improved service to America through enhanced alignment and strategic management of the Federal workforce.</p>' + '<iframe src="https://staging.d2d.gsa.gov/tableau-report-for-embed/11403" id="no-frame-border-1" frameborder="0" style="height:1130px;width:1050px;"></iframe>';
+
+function myFunction2() {
+  document.getElementById("wrapper").innerHTML = codeBlock2
+}
+
+var codeBlock2 =   '<h3>Cybersecurity - Manage Asset Security</h3>' + '<p>The IT Modernization CAP Goal works to build and maintain more modern, secure, and resilient information technology (IT) to enhance mission delivery and productivity – driving value by increasing efficiencies of Government IT spending while potentially reducing costs, increasing efficiencies, and enhancing citizen engagement and satisfaction with the services we provide.</p>' +
+'<iframe src="https://staging.d2d.gsa.gov/tableau-report-for-embed/11408" id="no-frame-border-1" frameborder="0" style="height:1130px;width:1050px;"></iframe>';
+
+function myFunction3() {
+  document.getElementById("wrapper").innerHTML = codeBlock3
+}
+
+var codeBlock3 =   '<h3>Cybersecurity - Limit Personnel Access</h3>' + '<p>The IT Modernization CAP Goal works to build and maintain more modern, secure, and resilient information technology (IT) to enhance mission delivery and productivity – driving value by increasing efficiencies of Government IT spending while potentially reducing costs, increasing efficiencies, and enhancing citizen engagement and satisfaction with the services we provide.</p>' +
+'<iframe src="https://staging.d2d.gsa.gov/tableau-report-for-embed/11409" id="no-frame-border-1" frameborder="0" style="height:1130px;width:1050px;"></iframe>';
+
+function myFunction4() {
+  document.getElementById("wrapper").innerHTML = codeBlock4
+}
+
+var codeBlock4 =   '<h3>Cybersecurity -Protect Networks and Data</h3>' + '<p>The IT Modernization CAP Goal works to build and maintain more modern, secure, and resilient information technology (IT) to enhance mission delivery and productivity – driving value by increasing efficiencies of Government IT spending while potentially reducing costs, increasing efficiencies, and enhancing citizen engagement and satisfaction with the services we provide.</p>' +
+'<iframe src="https://staging.d2d.gsa.gov/tableau-report-for-embed/11410" id="no-frame-border-1" frameborder="0" style="height:1130px;width:1050px;"></iframe>';
+
+function myFunction5() {
+  document.getElementById("wrapper").innerHTML = codeBlock5
+}
+
+var codeBlock5 =   '<h3>Cloud Email Adoption</h3>' + '<p>The IT Modernization CAP Goal works to build and maintain more modern, secure, and resilient information technology (IT) to enhance mission delivery and productivity – driving value by increasing efficiencies of Government IT spending while potentially reducing costs, increasing efficiencies, and enhancing citizen engagement and satisfaction with the services we provide.</p>' +
+'<iframe src="https://staging.d2d.gsa.gov/tableau-report-for-embed/11407" id="no-frame-border-1" frameborder="0" style="height:1130px;width:1050px;"></iframe>';
+
+function myFunction6() {
+  document.getElementById("wrapper").innerHTML = codeBlock6
+}
+
+var codeBlock6 =   '<h3>Category Management Data</h3>' + '<p>The Category Management CAP Goal works to leverage common contracts, meaning that they share in contracts that get the best value for taxpayers, in order to buy common goods and services as an enterprise. This approach will eliminate redundancies, increase efficiency, and deliver more value and savings from Federal acquisition programs. By the end of FY 2020, the Government will achieve $18 billion in savings for taxpayers by applying category management principles—or smart decision-making where agencies buy the same kinds of goods and services through best value contract solutions—to 60% of common spend. In addition, the Government will reduce duplicative contracts by 50,000, potentially reducing administrative costs by hundreds of millions of dollars.</p>' +
+'<iframe src="https://staging.d2d.gsa.gov/tableau-report-for-embed/10280" id="no-frame-border-1" frameborder="0" style="height:1130px;width:1050px;"></iframe>';
+
+function myFunction7() {
+  document.getElementById("wrapper").innerHTML = codeBlock7
+}
+
+var codeBlock7 =   '<h3>Customer Satisfaction Scores</h3>' + '<p>The Sharing Quality Services CAP Goal works to create a mission-driven government with modern technology and services that enable the workforce to better serve the American taxpayer.</p>' +
+'<iframe src="https://staging.d2d.gsa.gov/tableau-report-for-embed/11411" id="no-frame-border-1" frameborder="0" style="height:1130px;width:1050px;"></iframe>';
+
+function myFunction8() {
+  document.getElementById("wrapper").innerHTML = codeBlock8
+}
+
+var codeBlock8 =   '<h3>Cash Loss to the Government - FY 17-18</h3>' + '<p>The Getting Payments Right CAP Goal Demonstrate stewardship of taxpayer dollars by reducing monetary loss and making payments correctly the first time.</p>' +
+'<iframe src="https://staging.d2d.gsa.gov/tableau-report-for-embed/11405" id="no-frame-border-1" frameborder="0" style="height:1130px;width:1050px;"></iframe>';
+
+function myFunction9() {
+  document.getElementById("wrapper").innerHTML = codeBlock9
+}
+
+var codeBlock9 =   '<h3>Cash Loss to the Government - Program Scorecards</h3>' + '<p>The Getting Payments Right CAP Goal Demonstrate stewardship of taxpayer dollars by reducing monetary loss and making payments correctly the first time.</p>' +
+'<iframe src="https://staging.d2d.gsa.gov/tableau-report-for-embed/11406" id="no-frame-border-1" frameborder="0" style="height:1130px;width:1050px;"></iframe>';
