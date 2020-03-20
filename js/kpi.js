91851f03d8d09090a16451eef7bda15f88468120
Megan Looff <34106471+mlooff@users.noreply.github.com>
2020-03-20 11:01:12 -0400

    add FEVS data

--- js/kpi.js
+++ js/kpi.js
@@ -70,6 +70,13 @@ function myFunction1() {
 
 var codeBlock1 =   '<h3>Federal Employee Veiwpoint Survey - Strategic Hiring <em>(Updated December 2019)</em></h3>' + '<p>The Office of Personnel Management Federal Employee Viewpoint Survey measures employee perception of whether, and to what extent, conditions characteristic of successful organizations are present in their agencies.</p>' + '<p>The dashboard below compares 2017 and 2018 government-wide scores for two hiring-related metrics, tracking progress in hiring people with the right skills and overall satisfaction with agency hiring and recruiting. </p>'  + '<a class="usa-button usa-button-secondary" href="https://www.performance.gov/CAP/workforce/">View the CAP Goal</a>' + ' <a class="usa-button usa-button-secondary " href="https://www.performance.gov/CAP/KPI/FEVS-KPI-Data-dec.xlsx">Download Data</a>' + '<iframe src="https://d2d.gsa.gov/tableau-report-for-embed/8533" id="no-frame-border-1" frameborder="0" style="height:1130px;width:1050px;"></iframe>';
 
+function myFunction1a() {
+  document.getElementById("wrapper").innerHTML = codeBlock1a
+}
+
+var codeBlock1a =   '<h3>Federal Employee Veiwpoint Survey - Improve Employee Performance Management and Engagement <em>(Updated March 2020)</em></h3>' + '<p>The Office of Personnel Management Federal Employee Viewpoint Survey measures employee perception of whether, and to what extent, conditions characteristic of successful organizations are present in their agencies.</p>' + '<p>The dashboard below XXXXXXXXXX. </p>'  + '<a class="usa-button usa-button-secondary" href="https://www.performance.gov/CAP/workforce/">View the CAP Goal</a>' + ' <a class="usa-button usa-button-secondary " href="####">Download Data</a>' + '<iframe src="https://staging.d2d.gsa.gov/tableau-report-for-embed/11674" id="no-frame-border-1" frameborder="0" style="height:1130px;width:1050px;"></iframe>';
+
+
 function myFunction2() {
   document.getElementById("wrapper").innerHTML = codeBlock2
 }
