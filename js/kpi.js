b8526ed5a7d3dce172bcccd3379aff6754e2a37b
Megan Looff <34106471+mlooff@users.noreply.github.com>
2020-03-25 14:16:06 -0400

    Update kpi.js

--- js/kpi.js
+++ js/kpi.js
@@ -74,7 +74,7 @@ function myFunction1a() {
   document.getElementById("wrapper").innerHTML = codeBlock1a
 }
 
-var codeBlock1a =   '<h3>Federal Employee Veiwpoint Survey - Improve Employee Performance Management and Engagement <em>(Updated March 2020)</em></h3>' + '<p>The Office of Personnel Management Federal Employee Viewpoint Survey measures employee perception of whether, and to what extent, conditions characteristic of successful organizations are present in their agencies.</p>' + '<p>TThe dashboard below shows 2019 government-wide scores for one performance-related metric related to poor performers in work units.</p>' + '<a class="usa-button usa-button-secondary " href="https://www.performance.gov/CAP/KPI/KPI_FEVS_2019-Employee-Engagement">Download Data</a>' + '<iframe src="https://d2d.gsa.gov/tableau-report-for-embed/8689" id="no-frame-border-1" frameborder="0" style="height:1130px;width:1050px;"></iframe>';
+var codeBlock1a =   '<h3>Federal Employee Veiwpoint Survey - Improve Employee Performance Management and Engagement <em>(Updated March 2020)</em></h3>' + '<p>The Office of Personnel Management Federal Employee Viewpoint Survey measures employee perception of whether, and to what extent, conditions characteristic of successful organizations are present in their agencies.</p>' + '<p>The dashboard below shows 2019 government-wide scores for one performance-related metric related to poor performers in work units.</p>' + '<a class="usa-button usa-button-secondary " href="https://www.performance.gov/CAP/KPI/KPI_FEVS_2019-Employee-Engagement">Download Data</a>' + '<iframe src="https://d2d.gsa.gov/tableau-report-for-embed/8689" id="no-frame-border-1" frameborder="0" style="height:1130px;width:1050px;"></iframe>';
 
 
 function myFunction2() {
